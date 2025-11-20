4dc03c7604a6d8560623fc339e1a4d472088eb9e
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-11-20 16:00:03 +0000

    Create project directory

---
+++ ProductSystemCW/src/main/java/com/mycompany/productsystemcw/ProductSystemCW.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.productsystemcw;
+
+/**
+ *
+ * @author krasipetranov
+ */
+public class ProductSystemCW {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
