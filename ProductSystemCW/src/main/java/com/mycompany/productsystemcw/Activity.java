e15a94a7deb88eef0d9e4f873a4c8b70b43174a3
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-11-28 16:59:39 +0000

    Finishing Activity class

--- ProductSystemCW/src/main/java/com/mycompany/productsystemcw/Activity.java
+++ ProductSystemCW/src/main/java/com/mycompany/productsystemcw/Activity.java
@@ -4,10 +4,43 @@
  */
 package com.mycompany.productsystemcw;
 
+import java.util.Date;
+
 /**
  *
  * @author krasipetranov
  */
 public class Activity {
+    private final int activityID;
+    private final String activityName;
+    private final int activityQuantity;
+    private final Date activityDate;
     
+    public Activity(int activityID, String activityName, int activityQuantity, Date activityDate) {
+        this.activityID = activityID;
+        this.activityName = activityName;
+        this.activityQuantity = activityQuantity;
+        this.activityDate = activityDate;
+    }
+    
+    public int getActivityID() {
+        return activityID;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public int getActivityQuantity() {
+        return activityQuantity;
+    }
+
+    public Date getActivityDate() {
+        return activityDate;
+    }
+    
+    public String getActivityInfo() {
+        return "ID: " + activityID + ", Name: " + activityName +
+               ", Quantity: " + activityQuantity + ", Date: " + activityDate;
+    }
 }
