02140f91fd11680f322e846e1b18b363b9e0cb2a
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-11-29 21:45:48 +0000

    Creating ListNode class

---
+++ ProductSystemCW/src/main/java/com/mycompany/productsystemcw/ListNode.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.productsystemcw;
+
+/**
+ *
+ * @author krasipetranov
+ */
+public class ListNode {
+    public int data;
+    public ListNode next;
+    
+    public ListNode(int data) {
+        this.data = data;
+    }
+}
