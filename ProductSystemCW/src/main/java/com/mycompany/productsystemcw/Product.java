17d2cbc12be48aee940834de3d91e4d1f9732a02
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-11-30 20:46:05 +0000

    Create Product class

--- ProductSystemCW/src/main/java/com/mycompany/productsystemcw/Product.java
+++ ProductSystemCW/src/main/java/com/mycompany/productsystemcw/Product.java
@@ -11,5 +11,83 @@ import java.util.Date;
  * @author krasipetranov
  */
 public class Product {
+    private int productID;
+    private String productName;
+    private Date productEntryDate;
+    private int productQuantity;
+
     
+    private final ActivityLinkedList activities;
+
+   
+    public Product(int productID, String productName, Date productEntryDate, int productQuantity) {
+        this.productID = productID;
+        this.productName = productName;
+        this.productEntryDate = productEntryDate;
+        this.productQuantity = productQuantity;
+        this.activities = new ActivityLinkedList();
+    }
+
+    // Getters and Setters
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Date getProductEntryDate() {
+        return productEntryDate;
+    }
+
+    public void setProductEntryDate(Date productEntryDate) {
+        this.productEntryDate = productEntryDate;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public ActivityLinkedList getActivities() {
+        return activities;
+    }
+
+    
+    public void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+   
+    
+    public void removeActivity(int index) {
+        activities.remove(index);
+    }
+
+    
+    public void sortActivitiesByQuantity() {
+        activities.sortByQuantity();
+    }
+
+    public void printProduct() {
+        System.out.println("Product ID: " + productID);
+        System.out.println("Name: " + productName);
+        System.out.println("Entry Date: " + productEntryDate);
+        System.out.println("Quantity: " + productQuantity);
+        System.out.print("Activities: ");
+        activities.print();
+        System.out.println();
+    }
 }
