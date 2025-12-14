295060a26af279f828c025443b1acb90cace8530
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-12-14 15:36:06 +0000

    Add new CustomLinkedList class

---
+++ ProductSystemCW/src/main/java/com/mycompany/productsystemcw/CustomLinkedList.java
@@ -0,0 +1,149 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.productsystemcw;
+
+/**
+ *
+ * @author krasipetranov
+ */
+public class CustomLinkedList<T> {
+    private Node<T> head;
+    private int size;
+    private int capacity; 
+    
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    public CustomLinkedList() {
+        this.head = null;
+        this.size = 0;
+        this.capacity = Integer.MAX_VALUE; 
+    }
+    
+    public CustomLinkedList(int capacity) {
+        this.head = null;
+        this.size = 0;
+        this.capacity = capacity;
+    }
+    
+    
+    public void addFirst(T data) {
+        Node<T> newNode = new Node<>(data);
+        newNode.next = head;
+        head = newNode;
+        size++;
+        
+      
+        if (size > capacity) {
+            removeLast();
+        }
+    }
+    
+
+    public void addLast(T data) {
+        Node<T> newNode = new Node<>(data);
+        
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+        
+       
+        if (size > capacity) {
+            removeFirst();
+        }
+    }
+    
+
+    public T removeFirst() {
+        if (head == null) return null;
+        
+        T data = head.data;
+        head = head.next;
+        size--;
+        return data;
+    }
+    
+    
+    public T removeLast() {
+        if (head == null) return null;
+        
+        if (head.next == null) {
+            T data = head.data;
+            head = null;
+            size--;
+            return data;
+        }
+        
+        Node<T> current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        
+        T data = current.next.data;
+        current.next = null;
+        size--;
+        return data;
+    }
+    
+
+    public T get(int index) {
+        if (index < 0 || index >= size) return null;
+        
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+    
+    
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+ 
+    public int size() {
+        return size;
+    }
+    
+    
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        Node<T> current = head;
+        int index = 0;
+        
+        while (current != null) {
+            array[index++] = current.data;
+            current = current.next;
+        }
+        
+        return array;
+    }
+    
+
+    public void display() {
+        Node<T> current = head;
+        int index = 1;
+        while (current != null) {
+            System.out.println(index + ". " + current.data);
+            current = current.next;
+            index++;
+        }
+    }
+}
