0878073a3ae66d62a06eb7294e0248c6c26da08e
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-12-14 15:31:15 +0000

    Change Activity class

--- ProductSystemCW/src/main/java/com/mycompany/productsystemcw/Activity.java
+++ ProductSystemCW/src/main/java/com/mycompany/productsystemcw/Activity.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.productsystemcw;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 /**
@@ -11,42 +12,33 @@ import java.util.Date;
  * @author krasipetranov
  */
 public class Activity {
-    private final int activityID;
-    private final String activityName;
-    private final int activityQuantity;
-    private final Date activityDate;
+    private int activityId;
+    private String activityName; 
+    private int activityQuantity;
+    private LocalDate activityDate;
     
-    public Activity(int activityID, String activityName, int activityQuantity, Date activityDate) {
-        this.activityID = activityID;
+    public Activity(int activityId, String activityName, int activityQuantity, LocalDate activityDate) {
+        this.activityId = activityId;
         this.activityName = activityName;
         this.activityQuantity = activityQuantity;
         this.activityDate = activityDate;
     }
     
-    public int getActivityID() {
-        return activityID;
-    }
-
-    public String getActivityName() {
-        return activityName;
-    }
-
-    public int getActivityQuantity() {
-        return activityQuantity;
-    }
-
-    public Date getActivityDate() {
-        return activityDate;
-    }
+    public int getActivityId() { return activityId; }
+    public String getActivityName() { return activityName; }
+    public int getActivityQuantity() { return activityQuantity; }
+    public LocalDate getActivityDate() { return activityDate; }
     
-    public String getActivityInfo() {
-        return "ID: " + activityID + ", Name: " + activityName +
-               ", Quantity: " + activityQuantity + ", Date: " + activityDate;
-    }
+    public void setActivityId(int activityId) { this.activityId = activityId; }
+    public void setActivityName(String activityName) { this.activityName = activityName; }
+    public void setActivityQuantity(int activityQuantity) { this.activityQuantity = activityQuantity; }
+    public void setActivityDate(LocalDate activityDate) { this.activityDate = activityDate; }
     
     @Override
     public String toString() {
-    return "ID: " + activityID + ", Name: " + activityName +
-           ", Quantity: " + activityQuantity + ", Date: " + activityDate;
-}
+        return "Activity ID: " + activityId + 
+               ", Type: " + activityName + 
+               ", Quantity: " + activityQuantity + 
+               ", Date: " + activityDate;
+    }
 }
